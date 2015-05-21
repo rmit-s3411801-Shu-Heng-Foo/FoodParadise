3641bf30dffad24da2aa65964cad5f0cb8df628e
Shu-Heng FOO <s3411801@student.rmit.edu.au>
2015-05-21 13:08:34 +1000

    Startup code

---
+++ FoodParadise/FoodParadise/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  FoodParadise
+//
+//  Created by Henry Foo on 13/05/2015.
+//  Copyright (c) 2015 RMIT. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var signIn: UIButton!
+    @IBOutlet weak var signUp: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ FoodParadise/FoodParadise/SignUp.swift
@@ -0,0 +1,13 @@
+//
+//  SignUp.swift
+//  FoodParadise
+//
+//  Created by Henry Foo on 14/05/2015.
+//  Copyright (c) 2015 RMIT. All rights reserved.
+//
+
+import Cocoa
+
+class SignUp: UIViewController {
+
+}
